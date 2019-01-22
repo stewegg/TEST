43774f1daf178a886c1f97279010643f1158a91c
stewegg <45349707+stewegg@users.noreply.github.com>
2019-01-21 21:53:21 -0800

    Update DanL_env.py

--- gym_DanL/envs/DanL_env.py
+++ gym_DanL/envs/DanL_env.py
@@ -1,152 +1,119 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-"""
-Simulate the simplifie Banana selling environment.
-Each episode is selling a single banana.
-"""
 
-# core modules
-import logging.config
 import math
-import pkg_resources
-import random
-
-# 3rd party modules
-from gym import spaces
-import cfg_load
 import gym
+from gym import spaces, logger
+from gym.utils import seeding
 import numpy as np
-
-
-path = 'config.yaml'  # always use slash in packages
-filepath = pkg_resources.resource_filename('gym_DanL', path)
-config = cfg_load.load(filepath)
-logging.config.dictConfig(config['LOGGING'])
-
-
-def get_chance(x):
-    """Get probability that a banana will be sold at price x."""
-    e = math.exp(1)
-    return (1.0 + e) / (1. + math.exp(x + 1))
 
 
 class DanLEnv(gym.Env):
     """
-    Define a simple Banana environment.
-    The environment defines which actions can be taken at which point and
-    when the agent receives which reward.
+
+    Observation:
+        Type: Box(4)
+        Num	Observation                 Min         Max
+        0	Location A                   0           10
+        1	Location B                   0           10
+        2	Location C                   0           10
+        3	Location D                   0           10
+        4   k                            0           10
+    Actions:
+        Type: Discrete(2)
+        Num	Action
+        0	Push cart to the left
+        1	Push cart to the right
+
+
+
     """
 
-    def __init__(self):
-        self.__version__ = "0.1.0"
-        logging.info("DanLEnv - Version {}".format(self.__version__))
 
-        # General variables defining the environment
-        self.MAX_PRICE = 2.0
-        self.TOTAL_TIME_STEPS = 2
+    def __init__(self, aa, bb, cc, dd, kInput, timerInput, maxAxis):
 
-        self.curr_step = -1
-        self.is_banana_sold = False
+        self.visited=[]
+        self.xAxisSize=maxAxis
+        self.timer=timerInput
+        self.action_space = spaces.Discrete(2)
+        self.observation_space = spaces.Box(0, self.xAxisSize, dtype=np.float32)
 
-        # Define what the agent can do
-        # Sell at 0.00 EUR, 0.10 Euro, ..., 2.00 Euro
-        self.action_space = spaces.Discrete(21)
+        self.a = aa
+        self.b = bb
+        self.c = cc
+        self.d = dd
+        self.k = kInput
 
-        # Observation is the remaining time
-        low = np.array([0.0,  # remaining_tries
-                        ])
-        high = np.array([self.TOTAL_TIME_STEPS,  # remaining_tries
-                         ])
-        self.observation_space = spaces.Box(low, high, dtype=np.float32)
+        self.seed()
+        self.viewer = None
+        self.state = None
 
-        # Store what the agent tried
-        self.curr_episode = -1
-        self.action_episode_memory = []
+
+        self.steps_beyond_done = None
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
 
     def step(self, action):
-        """
-        The agent takes a step in the environment.
-        Parameters
-        ----------
-        action : int
-        Returns
-        -------
-        ob, reward, episode_over, info : tuple
-            ob (object) :
-                an environment-specific object representing your observation of
-                the environment.
-            reward (float) :
-                amount of reward achieved by the previous action. The scale
-                varies between environments, but the goal is always to increase
-                your total reward.
-            episode_over (bool) :
-                whether it's time to reset the environment again. Most (but not
-                all) tasks are divided up into well-defined episodes, and done
-                being True indicates the episode has terminated. (For example,
-                perhaps the pole tipped too far, or you lost your last life.)
-            info (dict) :
-                 diagnostic information useful for debugging. It can sometimes
-                 be useful for learning (for example, it might contain the raw
-                 probabilities behind the environment's last state change).
-                 However, official evaluations of your agent are not allowed to
-                 use this for learning.
-        """
-        if self.is_banana_sold:
-            raise RuntimeError("Episode is done")
-        self.curr_step += 1
-        self._take_action(action)
-        reward = self._get_reward()
-        ob = self._get_state()
-        return ob, reward, self.is_banana_sold, {}
+        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
+        state = self.state
+        A,B,C,D,k = state
+        self.time -= 1
 
-    def _take_action(self, action):
-        self.action_episode_memory[self.curr_episode].append(action)
-        self.price = ((float(self.MAX_PRICE) /
-                      (self.action_space.n - 1)) * action)
-
-        chance_to_take = get_chance(self.price)
-        banana_is_sold = (random.random() < chance_to_take)
-
-        if banana_is_sold:
-            self.is_banana_sold = True
-
-        remaining_steps = self.TOTAL_TIME_STEPS - self.curr_step
-        time_is_over = (remaining_steps <= 0)
-        throw_away = time_is_over and not self.is_banana_sold
-        if throw_away:
-            self.is_banana_sold = True  # abuse this a bit
-            self.price = 0.0
-
-    def _get_reward(self):
-        """Reward is given for a sold banana."""
-        if self.is_banana_sold:
-            return self.price - 1
+        if(k == A):
+            reward =1
+            self.visited.append(A)
+        elif (k == B):
+            reward = 1
+            self.visited.append(B)
+        elif (k == C):
+            reward = 1
+            self.visited.append(C)
+        elif (k == D):
+            reward = 1
+            self.visited.append(D)
         else:
-            return 0.0
+            reward = -1
+        self.state = (A, B, C, D, k)
+
+        for i in range(0,len(self.visited)):
+            if(self.visited[i]==A):
+                for j in range(i+1,len(self.visited)):
+                    if(self.visited[j]==B):
+                        for k in range(j + 1, len(self.visited)):
+                            if (self.visited[k] == C):
+                                for l in range(k + 1, len(self.visited)):
+                                    if (self.visited[l] == D):
+                                        done = True
+
+
+        if((A not in self.visited and B not in self.visited and C not in self.visited and D not in self.visited) or (self.timer==0)):
+            done=False
+
+        if done:
+            reward = 1.0
+        elif done==False:
+            reward=0
+        elif self.steps_beyond_done is None:
+            self.steps_beyond_done = 0
+            reward = 1.0
+        else:
+            if self.steps_beyond_done == 0:
+                logger.warn(
+                    "You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
+            self.steps_beyond_done += 1
+            reward = 0.0
+
+        return np.array(self.state), reward, done, {}
 
     def reset(self):
-        """
-        Reset the state of the environment and returns an initial observation.
-        Returns
-        -------
-        observation (object): the initial observation of the space.
-        """
-        self.curr_step = -1
-        self.curr_episode += 1
-        self.action_episode_memory.append([])
-        self.is_banana_sold = False
-        self.price = 1.00
-        return self._get_state()
+        self.state = self.np_random.uniform(low=0, high=self.k, size=(4,))
+        self.steps_beyond_done = None
+        return np.array(self.state)
 
-    def _render(self, mode='human', close=False):
-        return
-
-    def _get_state(self):
-        """Get the observation."""
-        ob = [self.TOTAL_TIME_STEPS - self.curr_step]
-        return ob
-
-    def seed(self, seed):
-        random.seed(seed)
-        np.random.seed
+    def render(self, mode='human'):
+        return None
+    def close(self):
+        if self.viewer:
+            self.viewer.close()
+            self.viewer = None
